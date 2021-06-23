f8fb81ca88a534229ca036af4f45153d863e6a13
teradaian <teradaian@gmail.com>
2021-06-23 07:25:11 -0700

    click handler by qs scaffolding

--- js/app.js
+++ js/app.js
@@ -1,10 +1,49 @@
+import deck from './deck.js'
+
+/*----- app's state (variables) -----*/ 
+
+let isWinner
+let turn
+
+let playerHand
+let computerHand
+let playerScore
+let computerScore
+let playerScorePile
+let computerScorePile
+// object? 
+let cardsPlayed
+
+
+/*----- cached element references -----*/ 
+
 const dayToggle = document.querySelector("#day-toggle")
+const playerHandEl = document.querySelector('.player-hand')
+const computerHandEl = document.querySelector('.computer-hand')
+const feildEl = document.querySelector('.field')
+const deckEl = document.querySelector('#deck')
+//drawer
+
+/*----- event listeners -----*/ 
 
 dayToggle.addEventListener('click', toggleTheme)
+playerHandEl.addEventListener('click', playCardHandler)
+feildEl.addEventListener('click', feildClickHandler)
+deckEl.addEventListener('click', deckClickHandler)
 
+/*----- functions -----*/
 
+function feildClickHandler(){
+    console.log(event.target.id)
+}
 
+function playCardHandler(){
+    console.log(event.target.id)
+}
 
+function deckClickHandler(){
+    console.log('deck clicked!')
+}
 
 
 
---
+++ js/deck.js
@@ -0,0 +1,3 @@
+const deck = {}
+
+export default deck;
