237aa4c7c70f224fd71a1d8e98e8303fc95bf5bc
teradaian <teradaian@gmail.com>
2021-06-24 07:10:29 -0700

    deck constructors deckjs

--- js/deck.js
+++ js/deck.js
@@ -1,3 +1,19 @@
-const deck = {}
+class Deck {
+    constructor() {
+      this.deck = [];
+  
+      const suits = ['Pine', 'Plum', 'Cherry', 'Wisteria', 'Iris', 'Peony', 'Clover', 'Grass', 'Chrysanthemum', 'Maple', 'Willow', 'Paulownia'];
+      const values = ['0', '1', '2', '3'];
+  
+      for (let suit in suits) {
+        for (let value in values) {
+          this.deck.push(`${values[value]} of ${suits[suit]}`);
+        }
+      }
+    }
+  }
+  
+  const deck1 = new Deck();
+  console.log(deck1.deck);
 
 export default deck;
