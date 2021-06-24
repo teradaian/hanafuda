871e38a39c39ac79e287e80efb9bdf904155b542
teradaian <teradaian@gmail.com>
2021-06-24 08:16:16 -0700

    dealing hand fix

--- js/deck.js
+++ js/deck.js
@@ -1,19 +1,67 @@
 class Deck {
     constructor() {
       this.deck = [];
-  
-      const suits = ['Pine', 'Plum', 'Cherry', 'Wisteria', 'Iris', 'Peony', 'Clover', 'Grass', 'Chrysanthemum', 'Maple', 'Willow', 'Paulownia'];
-      const values = ['0', '1', '2', '3'];
-  
+      this.reset();
+      this.shuffle();
+      this.deal();
+    }
+    
+    reset(){
+      this.deck = []
+
+      const suits = 
+      ['Pine', 'Plum', 'Cherry', 'Wisteria',
+      'Iris', 'Peony', 'Clover', 'Grass', 
+      'Chrysanthemum', 'Maple', 'Willow', 'Paulownia']
+
+      const values = ['0', '1', '2', '3']
+
       for (let suit in suits) {
         for (let value in values) {
-          this.deck.push(`${values[value]} of ${suits[suit]}`);
+          this.deck.push(`${suits[suit]}${values[value]}`)
         }
       }
     }
-  }
-  
-  const deck1 = new Deck();
-  console.log(deck1.deck);
 
-export default deck;
+    shuffle() {
+        const { deck } = this;
+        let m = deck.length, i;
+      
+        while (m) {
+          i = Math.floor(Math.random() * m--);
+      
+          [deck[m], deck[i]] = [deck[i], deck[m]];
+        }
+      
+        return this;
+      }
+
+    dealFeild(){
+        let feild = this.deck.splice(this.deck.length -8, 8)
+        console.log(feild)
+        return feild;
+    }
+
+    dealPlayerHand() {
+        let playerHand = this.deck.splice(this.deck.length -8, 8)
+        console.log(playerHand)
+        return playerHand;
+    }
+
+    dealComputerHand(){
+        let computerHand = this.deck.splice(this.deck.length -8, 8)
+        console.log(computerHand)
+        return computerHand;
+    }
+
+    deal(){
+        this.dealFeild()
+        this.dealPlayerHand()
+        this.dealComputerHand()
+    }
+
+}
+
+const deck1 = new Deck();
+
+console.log(deck1.deck)
