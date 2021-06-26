5a664d5f8002f5f0d3212115461e952dce6cd8d0
teradaian <teradaian@gmail.com>
2021-06-26 14:18:35 -0700

    scoring arrays added

---
+++ js/scoring.js
@@ -0,0 +1,25 @@
+
+// array of arrays, each array is a winning combo by NAME
+// loop through arrays, check if .every? === ? if so, add 1?
+
+//  that number will be the amount of 50 pts to add to total score
+
+// then array of arrays of 20, 10, 5pt values
+
+const yakuSets = [
+    ["Cherry0", "Grass0", "Chrysanthemum0"], 
+    ["Pine0", "Plum0", "Cherry0"],
+    ["Pine1", "Plum1", 'Cherry1'],
+    ["Peony1", "Chrysanthemum1", "Maple1"],
+    ["Peony0", "Chrysanthemum0", "Maple0"],
+    ["Wisteria1", "Iris1", "Clover1"],
+    ["Wisteria0", "Iris0", "Clover0"],
+    ["Clover0", "Grass1", "Maple0"]
+]
+
+const hikariBrights = ["Pawlownia0", "Willow0", "Grass0", "Cherry0", "Pine0"]
+
+const taneAnimals = ["Plum0", "Wisteria0", "Iris0", "Peony0", "Clover0", "Grass1", "Chrysanthemum0", "Maple0", "Willow1"]
+
+const tanzakuRibbons = ["Pine1", "Plum1", "Cherry1", "Wisteria1", "Iris1", "Peony1", "Clover1", "Chrysanthemum1", "Maple1", "Willow2"]
+
